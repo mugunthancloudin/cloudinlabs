c20f283bb003696730301cc3410b8361b5d09cf1
mugunthan <mugunthan@cloudinlabs.com>
2024-09-04 16:07:29 +0530

    navbar updated

--- src/pages/landingPage.jsx
+++ src/pages/landingPage.jsx
@@ -1,20 +1,22 @@
-import React from 'react'
-import Home from '../components/landingPage/home/home'
-import About from '../components/landingPage/about/about'
-import Service from '../components/landingPage/services/service'
-import Chooseus from '../components/landingPage/chooseus/chooseus'
-import Faq from '../components/landingPage/faq/faq'
-import Footer from '../components/header&footer/footer/footer'
+import React from "react";
+import Home from "../components/landingPage/home/home";
+import About from "../components/landingPage/about/about";
+import Service from "../components/landingPage/services/service";
+import Chooseus from "../components/landingPage/chooseus/chooseus";
+import Faq from "../components/landingPage/faq/faq";
+import Footer from "../components/header&footer/footer/footer";
+import Nav_bar from "../components/header&footer/header/header";
 
 export default function LandingPage() {
   return (
     <>
-     <Home/> 
-     <About/>
-     <Service/>
-     <Chooseus/>
-     <Faq/>
-     <Footer/>
+      <Nav_bar />
+      <Home />
+      <About />
+      <Service />
+      <Chooseus />
+      <Faq />
+      <Footer />
     </>
-  )
+  );
 }
